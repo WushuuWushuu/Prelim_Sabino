ad8b0383bb0f4e73d816778b8e74d44714cc13b2
WushuuWushuu <135561492+WushuuWushuu@users.noreply.github.com>
2024-02-23 14:14:27 +0800

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Web System Prelims</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+<header class="main_header">
+        <div class="header_top">
+            <h1 class="logo">CARSXXX</h1>
+            
+        </div>
+        <div class="header_content">
+            <h1>Nickolyte Klein C Sabino</h1>
+            <p>BSIT STUDENT</p>
+            <a href="#about" class="btn btn-xl">
+                Learn More
+                <i class="fas fa-chevron-right btn-icon"></i>
+            </a>
+        </div>
+           
+    </header>
+
+
+     
+
+    <section class="gallery">
+        <div class="gallery-container">    
+            
+            <a href="#">
+                <div class="img-container">
+                    <img src="../public/img/3.jpg">
+                    <div class="text-container">
+                      <p> Lamborghini</p>
+                    </div>
+                </div>
+                
+            </a>
+            
+            <a href="#">
+                <div class="img-container">
+                    <img src="../public/img/4.jpg" >
+                    <div class="text-container">
+                        <p> Porsche </p>
+                      </div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="img-container">
+                    <img src="../public/img/2.jpg" >
+                    <div class="text-container">
+                        <p> BMW </p>
+                      </div>
+                </div>       
+            </a>
+
+ 
+          
+        </div>
+    </section>
+
+
+   
+    <section class="section-b" id="about">
+        <div class="overlay">
+            <div class="section-b-inner"> <br><br><br><br>
+                <h3> Prelims Examination Done</h3>
+                <h2>Salamat sa lahat </h2>
+                <p>sorry sana mapatawad mo ko sir</p>
+            </div>
+        </div>
+    </section>
+
+
+   
+
+    
+
+
+    
+
+</body>
+</html>
+
+  
